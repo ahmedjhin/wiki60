b496d98cf68c7fd152c15cdc1edbbdf5f20bfce4
ahmedjhin <a7madti90@hotmail.com>
2023-03-08 23:03:58 +0400

    3ups

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+import markdown
 from . import util
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
 
 
 def index(request):
@@ -8,3 +17,26 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request,title):
+    html_content = convert_md_to_html(title)
+    if html_content == None:
+        return render(request,'encyclopedia/error.html',{
+            'message':' This entry does not exist'
+        })
+    else:
+        return render(request,'encyclopedia/entry.html',{
+            'title': title,
+            'content': html_content
+        })
+    
+
+def search(request):
+    if request.method == 'POST':
+        entry_search = request.POST['q']
+        html_contetn = convert_md_to_html(entry_search)
+        if html_contetn is not None:
+            return render(request, 'encyclopedia/entry.html',{
+                'title': entry_search,
+                "content": html_contetn
+            })
